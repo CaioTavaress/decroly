33f0aef9b51cc60db21aeca60c08b5701d44a822
Caio Francisco Tavares de Lima <56778786+CaioTavaress@users.noreply.github.com>
2024-09-30 09:38:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+  
+  
+  
+  function menuCall(c) {
+     let menuDisplay = document.querySelector('.menu');
+     
+     if (c == 0) {
+       menuDisplay.style.display = "block"
+     } else if (c == 1){
+       menuDisplay.style.display = "none"
+     }
+ }
